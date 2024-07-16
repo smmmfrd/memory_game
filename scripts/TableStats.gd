868457b6ac8c2541a28faca878f80748fb292c07
smmmfrd <smmmfrd@gmail.com>
2024-07-16 00:55:37 -0700

    Refactor and Logic: Extract Table Settings to Resource

---
+++ scripts/TableStats.gd
@@ -0,0 +1,6 @@
+class_name TableData
+
+extends Resource
+
+@export var table_size: Vector2i = Vector2i(2,2)
+@export var copies: int = 2
--- scripts/card_manager.gd
+++ scripts/card_manager.gd
@@ -4,8 +4,7 @@ var card_prefab = preload("res://card.tscn")
 
 @export var card_faces : Array[Texture2D]
 
-@export var table: Vector2i
-@export var copies: int = 2
+@export var table_data : TableData
 
 var check_val: String = ""
 var num_check: int = 0
@@ -26,12 +25,10 @@ func center_table():
 	# Set the table's position to half the board size, then add half a card in size
 	# With the cards' origin is at the center, and the first is made at the
 	# table's center, this calculation makes a perfect center.
-	var dest = Vector2(-(table.x * 0.5) * card_width + (card_width / 2),
-		-(table.y * 0.5) * card_height + (card_height / 2))
-	print(dest)
+	var dest = Vector2(-(table_data.table_size.x * 0.5) * card_width + (card_width / 2),
+		-(table_data.table_size.y * 0.5) * card_height + (card_height / 2))
 	
 	global_position = dest
-	print(global_position)
 
 func deal_table():
 	var deck : Array[Texture2D]
@@ -39,34 +36,34 @@ func deal_table():
 	var possible_cards = card_faces
 	possible_cards.shuffle()
 	
-	for i in range((table.x * table.y) / copies):
+	for i in range((table_data.table_size.x * table_data.table_size.y) / table_data.copies):
 		var new_card = possible_cards.pop_front()
 		
-		for j in copies:
+		for j in table_data.copies:
 			deck.append(new_card)
 	
 	deck.shuffle()
 	
-	for i in range(table.x * table.y):
+	for i in range(table_data.table_size.x * table_data.table_size.y):
 		deal_card(i, deck[i])
 
 func deal_card(index: int, card_face: Texture2D):
 	var instance : Card = card_prefab.instantiate()
 	instance.face_sprite = card_face
 	
-	var dealt_pos := Vector2(card_width * (index % table.x), card_height * floor(index /table.x ))
+	var dealt_pos := Vector2(card_width * (index % table_data.table_size.x),
+		card_height * floor(index / table_data.table_size.x ))
 	instance.global_position = self.global_position + dealt_pos
 	
 	add_child(instance)
 
 func check_card(card_face: String):
 	num_check += 1
-	print(num_check)
 	
 	if num_check == 1:
 		check_val = card_face
 	else:
-		if check_val == card_face and num_check == copies:
+		if check_val == card_face and num_check == table_data.copies:
 			clear_match(true)
 			num_check = 0
 		elif check_val != card_face:
